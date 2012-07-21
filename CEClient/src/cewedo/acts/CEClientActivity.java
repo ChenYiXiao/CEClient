7500d99bd2a1b6b5e944aeb69a1780d653c8d28b
陈一枭 <yhtt2020@foxmail.com>
2012-07-21 21:24:47 +0800

    初始化源码

---
+++ CEClient/src/cewedo/acts/CEClientActivity.java
@@ -0,0 +1,13 @@
+package cewedo.acts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CEClientActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
