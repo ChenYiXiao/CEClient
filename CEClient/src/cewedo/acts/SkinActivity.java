1a5fc3d0cd34726d1d4de9c52f3a226f0d111e35
陈一枭 <yhtt2020@foxmail.com>
2012-07-31 17:30:41 +0800

    完成皮肤的设置

--- CEClient/src/cewedo/acts/SkinActivity.java
+++ CEClient/src/cewedo/acts/SkinActivity.java
@@ -1,10 +1,17 @@
 package cewedo.acts;
 
 import cewedo.adapter.SkinItemAdapter;
+import cewedo.others.CEConfiguration;
+import cewedo.skn.SkinItem;
 import cewedo.skn.SkinManager;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.Toast;
 
 public class SkinActivity extends Activity {
 
@@ -15,8 +22,21 @@ public class SkinActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.skin);
-		GridView skinsGridView=(GridView)findViewById(R.id.gvNative);
+		final GridView skinsGridView=(GridView)findViewById(R.id.gvNative);
 		skinsGridView.setAdapter(new SkinItemAdapter(SkinManager.getAllNativeSkins(this), this));		
+		skinsGridView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				SkinItem skinItem=(SkinItem) skinsGridView.getAdapter().getItem(arg2);
+				CEConfiguration.setSkinName(SkinActivity.this, skinItem.getNameString());
+				CEConfiguration.setSkinPackageName(SkinActivity.this, skinItem.getPackageString());
+				Toast.makeText(SkinActivity.this, "当前皮肤已经切换成 "+CEConfiguration.getSkinName(SkinActivity.this)
+				, 2000).show();
+			}
+		});
+		
+		
 	}
 
 	
